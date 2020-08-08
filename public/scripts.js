74f5d699ef5151d43492bcc7b5b2ebe5b3065923
Paul K George <paulkottalil20@gmail.com>
2020-08-09 02:27:59 +0530

    video working

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,58 @@
 const socket = io('/')
-
-socket.emit('join-room',ROOM_ID, 10)
-
-socket.on('user-connected', userId => {
-  console.log("user connected", userId)  
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
+    host:'e1b601ab1a18.ngrok.io'
 })
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const peers = {}
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  addVideoStream(myVideo, stream)
+
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
+  })
+
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream)
+  })
+})
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video');
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+    console.log(video, stream);
+  video.srcObject = stream;
+  console.log(video.readyState);
+  
+  video.addEventListener('loadedmetadata', function(e){
+    video.play()
+  });
+  videoGrid.append(video)
+}
